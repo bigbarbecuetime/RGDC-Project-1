e0565da9e7a1c2e5f1cca2b31e2cf787a00b8a42
bigbarbecuetime <nesworkprof@gmail.com>
2024-03-18 02:28:23 -0400

    Added temp player controller

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -12,6 +12,18 @@ namespace RGDCP1.Player
         // TODO: Player Controller will eventually use a Finite State Machine to track its current state. (Ex. Falling, Running)
         // TODO: Ensure Player Controller feels good to use during gameplay.
 
+        // TODO: Player gets stuck against walls when moving into them, this may require the method of moving the player to be redesigned.
+
+        /// <summary>
+        /// The minimum value that can be entered in unity's inspector for the script
+        /// </summary>
+        private const int MIN_ATTRIBUTE_VALUE = 1;
+
+        /// <summary>
+        /// Value used to determine if moving left or right in the x axis.
+        /// </summary>
+        private float xMovementAxis = 0;
+
         /// <summary>
         /// Rigidbody used for the player's movement.
         /// </summary>
@@ -22,18 +34,20 @@ namespace RGDCP1.Player
         /// The force the player will experience when jumping.
         /// </summary>
         [SerializeField]
-        private float jumpForce = 0f;
+        [Min(MIN_ATTRIBUTE_VALUE)]
+        private float jumpForce;
 
         /// <summary>
         /// Maximum velocity player can have.
         /// </summary>
         [SerializeField]
-        private float maxVelocity = 0f;
+        [Min(MIN_ATTRIBUTE_VALUE)]
+        private float maxVelocity;
 
         /// <summary>
         /// Player will jump, called by event from player input component.
         /// </summary>
-        void OnJump()
+        public void OnJump()
         {
             // TODO: Only allow jumps when touching (Or very close) to the ground.
             playerRigidbody.AddForce(Vector3.up * jumpForce);
@@ -42,10 +56,19 @@ namespace RGDCP1.Player
         /// <summary>
         /// Player will move, direction depending on the input axis's value.
         /// </summary>
-        private void OnMove(InputAction.CallbackContext context)
+        public void OnMove(InputAction.CallbackContext context)
         {
-            // TODO: 
-            playerRigidbody.velocity = context.ReadValue<Vector3>() * maxVelocity;
+            // HACK: This design does not seem good, potentially update movement some other way.
+            xMovementAxis = context.ReadValue<float>();
+        }
+
+        /// <summary>
+        /// FixedUpdated called by Unity's physics system.
+        /// </summary>
+        public void FixedUpdate()
+        {
+            // Move player depending on x axis input
+            playerRigidbody.velocity = new Vector2(xMovementAxis * maxVelocity, playerRigidbody.velocity.y);
         }
     }
 }

b6fb2f242afb6bdc7a43a69bf4ab54f01370aa8b
bigbarbecuetime <nesworkprof@gmail.com>
2024-03-31 04:33:51 -0400

    Player Controller - Constrains max velocity properly - Fixed movement bug involving being overspeed
    Added GreySlope Tile for prototyping

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -10,9 +11,10 @@ namespace RGDCP1.Player
     public class PlayerController : MonoBehaviour
     {
         // TODO: Player Controller will eventually use a Finite State Machine to track its current state. (Ex. Falling, Running)
-        // TODO: Ensure Player Controller feels good to use during gameplay.
-        // TODO: Seperate Camera controller into its own class
-        // TODO: Player gets stuck against walls when moving into them, this may require the method of moving the player to be redesigned.
+        // TODO: Add support for acceleration "functions", exponential, logarithmic, linear
+        // TODO: Ensure encapsulation
+        // TODO: Refactor
+        // TODO: Extract x component of velocity as its own vector without the y
 
         /// <summary>
         /// The minimum value that can be entered in unity's inspector for the script
@@ -23,6 +25,11 @@ namespace RGDCP1.Player
         /// Value used to determine if moving left or right in the x axis.
         /// </summary>
         private float xMovementAxis = 0;
+
+        /// <summary>
+        /// Threashold to see when we need to slow down the player automatically, when landed and no input is pressed, calculated based on deceleration
+        /// </summary>
+        private float slowdownThreshold;
 
         /// <summary>
         /// Rigidbody used for the player's movement.
@@ -41,35 +48,48 @@ namespace RGDCP1.Player
         /// </summary>
         [SerializeField]
         [Min(MIN_ATTRIBUTE_VALUE)]
-        private float jumpForce;
+        public float jumpAcceleration;
 
         /// <summary>
         /// Maximum velocity player can have.
         /// </summary>
         [SerializeField]
         [Min(MIN_ATTRIBUTE_VALUE)]
-        private float maxVelocity;
+        public float maxVelocity;
 
         /// <summary>
         /// Acceleration in m/s^2
         /// </summary>
         [Min(MIN_ATTRIBUTE_VALUE)]
-        public float acceleration;
+        public float airAcceleration;
 
         /// <summary>
         /// Deceleration in m/s^2
         /// </summary>
         [Min(MIN_ATTRIBUTE_VALUE)]
-        public float deceleration;
+        public float airDeceleration;
+
+        /// <summary>
+        /// Acceleration in m/s^2
+        /// </summary>
+        [Min(MIN_ATTRIBUTE_VALUE)]
+        public float groundAcceleration;
+
+        /// <summary>
+        /// Deceleration in m/s^2
+        /// </summary>
+        [Min(MIN_ATTRIBUTE_VALUE)]
+        public float groundDeceleration;
 
         /// <summary>
         /// Player will jump, called by event from player input component.
         /// </summary>
-        public void OnJump()
+        public void OnJump(InputAction.CallbackContext context)
         {
-            // TODO: Only allow jumps when touching (Or very close) to the ground.
+            float value = context.ReadValue<float>();
+            // TODO: Only allow jumps when touching (Or very close) to the ground (Buffered).
             // TODO: Jump height should be variable depending on how long you hold jump
-            playerRigidbody.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
+            if (value > 0) playerRigidbody.AddForce(Vector3.up * jumpAcceleration * value * playerRigidbody.mass, ForceMode2D.Impulse);
         }
 
         /// <summary>
@@ -79,13 +99,11 @@ namespace RGDCP1.Player
         {
             xMovementAxis = context.ReadValue<float>();
         }
-        float debugVel = 0;
 
         public void Update()
         {
-            // Draw the current velocity
+            // Draw the current velocity for debugging
             Debug.DrawRay(playerRigidbody.position, (playerRigidbody.velocity / maxVelocity) * Camera.main.orthographicSize);
-            Debug.DrawRay(playerRigidbody.position + new Vector2(0, 1.5f), (new Vector2(debugVel,0) / maxVelocity) * Camera.main.orthographicSize);
         }
 
         /// <summary>
@@ -94,52 +112,51 @@ namespace RGDCP1.Player
         public void FixedUpdate()
         {
             // Move player depending on x axis input
-            // TODO: This is still WIP, need to cap the change in velocity, while allowing for objects to hit the player with uncapped delta v
-            // HACK: Ideally, the input speed should change between states, meaning when in air, you are only allowed to +-2m/s of force, while landed could be +-10m/s
             // Note when moving the player, it is better to use the rigidbody's position, not the transforms
-            // Potentially track a "Speed effect" velocity, then once forces reach the maximum speed effect, stop applying them
-            // TODO: update to use acceleration
-            // TODO update to calculate velocity effect and limit max velocity effect
-
-            //DebugMovement();
             PhysMovement();
         }
 
-        /// <summary>
-        /// Value used as a timer for debugging 
-        /// </summary>
-        private float debugTimer = 0;
-        /// <summary>
-        /// Used to verify acceleration and velocity values used in the velocity effect section
-        /// </summary>
-        private void DebugMovement()
-        {
-            playerRigidbody.AddForce(new Vector2(acceleration * playerRigidbody.mass, 0));
-            debugTimer += Time.fixedDeltaTime;
-
-            if (debugTimer * acceleration >= maxVelocity)
-            {
-                Debug.Log("Time to max velocity: " + debugTimer + " seconds");
-                Debug.Log("Calculated velocity: " + debugTimer * acceleration);
-                Debug.Log("Actual velocity: " + playerRigidbody.velocity);
-                Time.timeScale = 0;
-            }
-        }
-       
+        // TODO: Refactor to use and define a series of states
         private void PhysMovement()
         {
-            // Deceleration (If trying to move opposite to current velocity direction)
-            if (xMovementAxis * -1 == Mathf.Ceil(playerRigidbody.velocity.normalized.x))
+            // Currently exponential
+            float acceleration = groundAcceleration*groundAcceleration;
+            float deceleration = groundDeceleration*groundDeceleration;
+
+            // Calculate the threshold to avoid overshooting auto slow down
+            slowdownThreshold = deceleration * Time.fixedDeltaTime;
+
+            // Dot product, 1 being the same direction, 0 being perpendicular, -1 being oposites
+            float currentDirection = Vector2.Dot(new Vector2(playerRigidbody.velocity.x,0).normalized, Vector2.right);
+            
+            if (xMovementAxis == 0)
             {
-                playerRigidbody.AddForce(new Vector2(deceleration * playerRigidbody.mass * xMovementAxis, 0));
+                // TODO: should check if in air, only want this when grounded
+                if (Mathf.Abs(playerRigidbody.velocity.x) >= slowdownThreshold)
+                {
+                    playerRigidbody.AddForce(new Vector2(deceleration * playerRigidbody.mass * currentDirection * -1, 0));
+                }
+                // Ensures if we are under the threashold, we should be stopped, avoids jittering
+                else if (Mathf.Abs(playerRigidbody.velocity.x) < slowdownThreshold && playerRigidbody.velocity.x != 0)
+                { 
+                    playerRigidbody.velocity = new Vector2(0, playerRigidbody.velocity.y);
+                }
+            }
+            // Deceleration (If trying to move opposite to current velocity direction)
+            else if (xMovementAxis * -1 == currentDirection)
+            {
+               playerRigidbody.AddForce(new Vector2(deceleration * playerRigidbody.mass * xMovementAxis, 0));
             }
             // Acceleration, checking if we are overspeed
             else if (Mathf.Abs(playerRigidbody.velocity.x) < maxVelocity)
             {
-                playerRigidbody.AddForce(new Vector2(acceleration * playerRigidbody.mass * xMovementAxis, 0));
-            }
-
-            // TODO: Add slowdown when no input
+                float force = acceleration * playerRigidbody.mass;
+                if (force / playerRigidbody.mass * Time.fixedDeltaTime + Mathf.Abs(playerRigidbody.velocity.x) > maxVelocity)
+                {
+                    force = ((maxVelocity - Mathf.Abs(playerRigidbody.velocity.x)) / Time.fixedDeltaTime) * playerRigidbody.mass;
+                }
+                playerRigidbody.AddForce(new Vector2(force*xMovementAxis, 0));
+            }          
         }
     }
 }
